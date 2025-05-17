8cd2cbe7efff93811b44a37867721ce31601dde3
Santiago <santiagomancomaya@gmail.com>
2025-05-17 18:31:57 -0500

    ADD: Student reg app

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'studentreg', # Custom app para registro de estudiantes
 ]
 
 MIDDLEWARE = [
